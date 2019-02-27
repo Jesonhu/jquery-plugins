79a172f1be5303d315b0c040a0d7382e3952afe4
Jeson <jesonhu_web@163.com>
2019-02-27 16:06:02 +0800

    feat: hoverWrap 插件完成 o(￣▽￣)ｄ

--- plugins/hover-wrap/resources/js/jquery.hoverWrap.js
+++ plugins/hover-wrap/resources/js/jquery.hoverWrap.js
@@ -9,18 +9,107 @@
 ;(function(win, doc, $) {
   const pluginName = 'hoverWrap';
   const Plugin = function(el, options) {
-
+    this.init(el, options);
   }
   Plugin.prototype = {
     constructor: Plugin,
-    init() {
-
+    init(el, options) {
+      this.initData(el, options);
     },
-    initData() {
-
+    
+    initData(el, options) {
+      this.initOptions(options);
+      this.initPosData();
+      this.initView(el);
+      this.initEvent();
+    },
+    initOptions(options) {
+      this._defaultOptions = {
+        from: 'top',
+        speed: 250,
+        percent: 0.4,
+      }
+      this._options = $.extend({}, this._defaultOptions, options);
+    },
+    initPosData() {
+      const from = this._options.from;
+      switch (from) {
+        case 'top':
+          this.pos1 = 'top';
+          this.pos2 = 'left';
+          this.movePos = 'top';
+          this.size1 = 'height';
+          this.size2 = 'width';
+          break;
+        case 'bottom':
+          this.pos1 = 'bottom';
+          this.pos2 = 'left';
+          this.movePos = 'bottom';
+          this.size1 = 'height';
+          this.size2 = 'width';
+          break;
+        case 'left':
+          this.pos1 = 'left';
+          this.pos2 = 'top';
+          this.movePos = 'left';
+          this.size1 = 'width';
+          this.size2 = 'height';
+          break;
+        case 'right':
+          this.pos1 = 'right';
+          this.pos2 = 'top';
+          this.movePos = 'right';
+          this.size1 = 'width';
+          this.size2 = 'height';
+          break;
+      }
     },
     initEvent() {
+      this.evnetHandle();
+    },
+    initView(el) {
+      this._$el = $(el);
+      this._$hoverWrap = this._$el.find('.hover-wrap');
+      /** 文字容器所在的比例 */
+      const n = this.moveSize = parseFloat(this._$el.height()) * this._options.percent;
+      const pos1 = this.pos1;
+      const pos2 = this.pos2;
+      const size1 = this.size1;
+      const size2 = this.size2;
+      this._$el.css({
+        position: 'relative',
+        overflow: 'hidden',
+        cursor: 'pointer'
+      });
+      this._$hoverWrap.css({
+        position: 'absolute',
+        zIndex: 999999,
+        [pos1]: '-' + n + 'px',
+        [pos2]: 0,
+        [size1]: n + 'px',
+        [size2]: '100%'
+      })
+    },
+    get options() {
+      return this._options;
+    },
+    evnetHandle() {
+      const $el = this._$el;
+      const $hoverWrap = this._$hoverWrap;
+      const movePos = this.movePos;
+      const speed = this._options.speed;
+      const n = this.moveSize;
 
+      $el.on('mouseenter', function() {
+        $hoverWrap.stop().animate({
+          [movePos]: 0
+        }, speed);
+      });
+      $el.on('mouseleave', function() {
+        $hoverWrap.stop().animate({
+          [movePos]: '-'+ n + 'px'
+        }, speed);
+      });
     }
   }
 
@@ -29,7 +118,7 @@
     const $this = $(this);
     return $this.each((index, item) => {
       const $item = $(item);
-      new Plugin($item, options);
+      const plugin = new Plugin($item, options);
     });
   }
 
